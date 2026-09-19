9f70fa8fddc68f111665449c26c849e4ec2e8eb2
Blowerlop <Blowerlop@users.noreply.github.com>
2026-09-20 01:42:46 +0200

    Automator: Daily automator fully functional without doing anything

--- src/Automator/DailyAutomator.cpp
+++ src/Automator/DailyAutomator.cpp
@@ -11,46 +11,39 @@ void DailyAutomator::Update(boost::coroutines2::coroutine<void>::push_type& yiel
     const auto& applicationManager = ApplicationManager::Instance();
     cv::Point coordinate;
 
-    // This is the coordinate of the top banner.
-    // Try to open it. Maybe the user has already opened it so it will closes, but it's ok because we are going to open it later.
-    applicationManager.PointAndClick(cv::Point(466, 231));
-    yield();
 
-    if (TemplateMatching::Match(gameScreenshot, "assets/Quests/VeryHard.jpg", coordinate))
+    while (!(TemplateMatching::Match(gameScreenshot, "assets/Quests/VeryHard.jpg", coordinate) || TemplateMatching::Match(gameScreenshot, "assets/Quests/Normal.jpg", coordinate)))
     {
-        applicationManager.PointAndClick(coordinate);
-        yield();
-    }
-
-    if (TemplateMatching::Match(gameScreenshot, "assets/Quests/Normal.jpg", coordinate))
-    {
-        applicationManager.PointAndClick(coordinate);
+        // This is the coordinate of the top banner.
+        // Try to open it. Maybe the user has already opened it so it will closes, but it's ok because we are going to open it later.
+        applicationManager.PointAndClick(cv::Point(466, 231));
         yield();
 
-    }
-
-    if (TemplateMatching::Match(gameScreenshot, "assets/Quests/PrepareForQuest.jpg", coordinate))
-    {
-        applicationManager.PointAndClick(coordinate);
-        currentQuest++;
-    }
-
-    const int currentTeam = Settings::dailyTeam[currentQuest];
-    const std::string filePath = "assets/Team/" + std::to_string(currentTeam) + ".jpg";
-    for (int i = 0; i < currentTeam; i++)
-    {
-        if (TemplateMatching::Match(gameScreenshot, filePath, coordinate))
+        if (TemplateMatching::Match(gameScreenshot, "assets/Quests/StartQuest.jpg", coordinate))
         {
-            applicationManager.PointAndClick(coordinate);
+            goto questSelected;
         }
-        else
-        {
-            TemplateMatching::Match(gameScreenshot, "assets/Team/Next.jpg", coordinate);
-            applicationManager.PointAndClick(coordinate);
-        }
+    }
+    applicationManager.PointAndClick(coordinate);
 
+    while (!TemplateMatching::Match(gameScreenshot, "assets/Quests/PrepareForQuest.jpg", coordinate)) yield();
+    applicationManager.PointAndClick(coordinate);
+    currentQuest++;
+
+    // Reset so we don't click on anything important by accident
+    coordinate = cv::Point(0, 0);
+
+    questSelected:
+    const int requestedTeam = Settings::dailyTeam[currentQuest];
+    const std::string requestedTeamFilePath = "assets/Team/" + std::to_string(requestedTeam) + ".jpg";
+    while (!TemplateMatching::Match(gameScreenshot, requestedTeamFilePath, coordinate))
+    {
+        TemplateMatching::Match(gameScreenshot, "assets/Team/Next.jpg", coordinate);
+        applicationManager.PointAndClick(coordinate);
         yield();
     }
+
+    applicationManager.PointAndClick(coordinate);
 
     if (Settings::useStatsBoost.load() && TemplateMatching::Match(gameScreenshot, "assets/Quests/Solo/UseStatsBoost.jpg", coordinate))
     {
@@ -72,24 +65,31 @@ void DailyAutomator::Update(boost::coroutines2::coroutine<void>::push_type& yiel
         }
     }
 
-    if (TemplateMatching::Match(gameScreenshot, "assets/Quests/StartQuest.jpg", coordinate))
+    // When we click, we can can be notify that we have no more tickets and it will switch to the BuySoulTicketsAutomator.
+    // After that, if we not loop, we are stuck because we had already click on the "Start Quest" button.
+    // So we need to re-click until we past the "Start Quest" screen.
+    while (TemplateMatching::Match(gameScreenshot, "assets/Quests/StartQuest.jpg", coordinate))
     {
         applicationManager.PointAndClick(coordinate);
+        yield();
     }
 
     while (!TemplateMatching::Match(gameScreenshot, "assets/Quests/TapScreen.jpg", coordinate)) yield();
     applicationManager.PointAndClick(coordinate);
 
+    acceptRewards:
     if (TemplateMatching::Match(gameScreenshot, "assets/Quests/Close.jpg", coordinate))
     {
         applicationManager.PointAndClick(coordinate);
-        return;
+        yield();
     }
 
-    if (TemplateMatching::Match(gameScreenshot, "assets/Quests/Retry.jpg", coordinate))
+    while (!TemplateMatching::Match(gameScreenshot, "assets/Quests/Retry.jpg", coordinate))
     {
-        applicationManager.PointAndClick(coordinate);
+        yield();
+        goto acceptRewards;
     }
+    applicationManager.PointAndClick(coordinate);
 }
 
 std::string DailyAutomator::ToString() const
